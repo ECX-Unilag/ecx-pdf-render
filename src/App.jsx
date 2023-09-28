2493fd162b4c6d4ae0e9544a44eb0b6d6f01b46a
coded1guy <grggmmnl@gmail.com>
2023-09-28 16:06:14 +0100

    feat: created a pdf viewer

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import "./App.css";
+
+function getWindowSize() {
+  const { innerWidth, innerHeight } = window;
+  return { innerWidth, innerHeight };
+}
+
+const App = () => {
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  useEffect(() => {
+    function handleWindowResize() {
+      setWindowSize(getWindowSize());
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  return (
+    <div className="pdfReader">
+      <nav className="navbar">
+        <button
+          onClick={() => {
+            setPageNumber(pageNumber - 1);
+            console.log(pageNumber);
+          }}
+          disabled={pageNumber > 1 ? false : true}
+        >
+          Previous
+        </button>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+        <button
+          onClick={() => {
+            setPageNumber(pageNumber + 1);
+            console.log(pageNumber);
+          }}
+          disabled={pageNumber < numPages ? false : true}
+        >
+          Next
+        </button>
+      </nav>
+      <Document
+        width={windowSize.innerWidth < 780 ? windowSize.innerWidth - 80 : 700}
+        file="/ECX-4.0-Sponsorship-Packet.pdf"
+        onLoadSuccess={onDocumentLoadSuccess}
+      >
+        <Page
+          width={windowSize.innerWidth < 780 ? windowSize.innerWidth - 80 : 700}
+          pageNumber={pageNumber}
+          renderAnnotationLayer={false}
+          renderTextLayer={false}
+        />
+      </Document>
+    </div>
+  );
+};
+
+export default App;
